78f3788b1c1841e88b705487cd72fe9d741b17b6
gsim1903 <gavin@internetbusker.co>
2022-05-19 10:03:46 +0200

    article association to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-    validates_presence_of :body
-    belongs_to :article 
+  validates_presence_of :body
+  belongs_to :article
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe Comment, type: :model do
+  describe 'Database table' do
+    it { is_expected.to have_db_column :body }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :body }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :article }
+  end
+end
